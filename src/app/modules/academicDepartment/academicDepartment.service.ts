d4f8834cfeefbc8306eead16c16a19d06d4b6e75
khairul-islam98 <janeyrahman900@gmail.com>
2024-05-29 02:46:50 +0600

    create academic department services

---
+++ src/app/modules/academicDepartment/academicDepartment.service.ts
@@ -0,0 +1,32 @@
+import { TAcademicDepartment } from './academicDepartment';
+import { AcademicDepartment } from './academicDepartment.model';
+
+const createAcademicDepartmentIntoDB = async (payload: TAcademicDepartment) => {
+  const result = await AcademicDepartment.create(payload);
+  return result;
+};
+const getAllAcademicDepartmentFromDB = async () => {
+  const result = await AcademicDepartment.find();
+  return result;
+};
+const getSingleAcademicDepartmentFromDB = async (id: string) => {
+  const result = await AcademicDepartment.findById(id);
+  return result;
+};
+const updateAcademicDepartmentFromDB = async (
+  id: string,
+  payload: Partial<TAcademicDepartment>,
+) => {
+  const result = await AcademicDepartment.findOneAndUpdate(
+    { _id: id },
+    payload,
+    { new: true },
+  );
+  return result;
+};
+export const AcademicDepartmentServices = {
+  createAcademicDepartmentIntoDB,
+  getAllAcademicDepartmentFromDB,
+  getSingleAcademicDepartmentFromDB,
+  updateAcademicDepartmentFromDB,
+};
