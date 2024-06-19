d4daed71eca6c9d27fd5748897e1fc1da94f0531
khairul-islam98 <janeyrahman900@gmail.com>
2024-06-20 00:01:33 +0600

    update querybuilder

--- src/app/modules/students/student.service.ts
+++ src/app/modules/students/student.service.ts
@@ -32,8 +32,12 @@ const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
     .sort()
     .paginate()
     .fields();
+  const meta = await studentQuery.countTotal();
   const result = await studentQuery.modelQuery;
-  return result;
+  return {
+    meta,
+    result,
+  };
 };
 
 const getSingleStudentFromDB = async (id: string) => {
